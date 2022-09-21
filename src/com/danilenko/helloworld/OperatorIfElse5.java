ca15c715c298d655b6070b7fda36ca427c613c07
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-09-21 19:52:33 +0300

    Operator if and else - 5
    Zlatopolsky D.M.
    Collection of programming tasks. 2011
    page 30
    Subject: Conditional operator.
    Task 4.21
    
    https://school272.ucoz.ru/TeachersWorks/Kononov/sbornik_zadach_po_programmiroaniyu.pdf

---
+++ src/com/danilenko/helloworld/OperatorIfElse5.java
@@ -0,0 +1,25 @@
+package com.danilenko.helloworld;
+
+import java.util.Scanner;
+
+public class OperatorIfElse5 {
+
+    public static void main(String[] args) {
+
+        Scanner a0 = new Scanner(System.in);
+        System.out.print("Enter divisor 'a': ");
+        double a = a0.nextDouble();
+        Scanner b0 = new Scanner(System.in);
+        System.out.print("Enter dividend 'b': ");
+        double b = b0.nextDouble();
+
+        if(b % a == 0){
+            System.out.println("\n" + "a is 'a' divisor of 'b'");
+        }
+        else{
+            System.out.println("\n" + "'a' is not a divisor of 'b'");
+        }
+
+    }
+
+}
