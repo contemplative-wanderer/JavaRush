662593497776fbedd14e0fbacdd3d9a39527e0ad
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-09-27 23:40:32 +0300

    Operator if

--- src/com/danilenko/helloworld/JavaRush/Lev3/Lec2.java
+++ src/com/danilenko/helloworld/JavaRush/Lev3/Lec2.java
@@ -1,23 +1,21 @@
 package com.danilenko.helloworld.JavaRush.Lev3;
 
-import java.util.Scanner;
-
 /*Передать на вход программы число в качестве аргумента.
 Если оно нечетное, распечатать его.
 Используем оператор if.
 Используйте метод Integer.parseInt() для преобразования из String в int.*/
+// Ряд чисел - 3 5 12 15 22
 
+import java.util.Scanner;
 
-public class Lect4_1 {
+public class Lec2 {
 
     public static void main(String[] args) {
 
-        System.out.println(args[0]);
-        int number = Integer.parseInt(args[0]);
+        int number = Integer.parseInt(args[3]);
         if (number % 2 != 0) {
+            System.out.println(number);
 
         }
-
     }
-
 }
