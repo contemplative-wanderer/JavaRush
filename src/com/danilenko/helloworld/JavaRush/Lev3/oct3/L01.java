31df052ecacdf3b2c99409703a82c2f9cfc4e062
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-10-04 23:58:28 +0300

    Ruchnoi poisk zagadannogo chisla.

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/oct3/L01.java
@@ -0,0 +1,25 @@
+package com.danilenko.helloworld.JavaRush.Lev3.oct3;
+
+import java.util.Scanner;
+
+/* Задать число. Потом перебирая угадывать какое число задано. После введенния загадоноого числа вывести на экран это число и завершить выполнение программы.*/
+
+public class L01 {
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+        System.out.print("Загадай число: ");
+        int number = scanner.nextInt();
+
+        while (true) {
+            int inputNumber = scanner.nextInt();
+            if (inputNumber == number) {
+                System.out.print("Загадочное число равно: " + number);
+                break;
+            } else {
+                System.out.println("Попробуй еще раз");
+            }
+        }
+    }
+}
