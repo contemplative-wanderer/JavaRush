f86a462c08e662fb7d4ad389650af5cc67afd9d2
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-10-25 21:36:39 +0300

    Chetnye i nechetnye jachejki massiva

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/oct25/L01.java
@@ -0,0 +1,29 @@
+package com.danilenko.helloworld.JavaRush.Lev3.oct25;
+
+/* Четные и нечетные ячейки массива
+В методе main(String[]) тебе нужно заполнить массив strings значениями. Если индекс массива чётный —
+присвоить значение "Чётный" (ноль — цифра чётная), иначе присвоить "Нечётный". Вывод текста в консоль в тестировании не участвует.
+Требования:
+В классе Solution должна быть публичная статическая переменная strings типа String[], проинициализированная массивом размером 5 элементов.
+Метод main(String[]) должен заполнять массив согласно условию.*/
+
+public class L01 {
+
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < strings.length; i++) {
+
+            if (i % 2 != 0) {
+                strings[i] = ODD;
+            }else{
+                strings[i] = EVEN;
+            }
+            System.out.print("index = " + i);
+            System.out.println(" value = " + strings[i]);
+        }
+    }
+}
