3b88e3e5bda1007a6b5bb38897980f210ed5adf4
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-10-27 01:39:44 +0300

    Raschirenie vozmognostei - chisla s minusom

--- src/com/danilenko/helloworld/JavaRush/Lev3/oct27/L01.java
+++ src/com/danilenko/helloworld/JavaRush/Lev3/oct27/L01.java
@@ -11,16 +11,19 @@ import java.util.Scanner;
 
 public class L01 {
 
+    public static int[] array;
     public static void main(String[] args) {
 
         var scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int [] array = new int[n];
-        int max = 0;
+        array = new int[n];
 
         for (int i = 0; i < n; i++) {
             array [i] = scanner.nextInt();
-            if (max < array[i]) {
+        }
+        int max = array[0];
+        for (int i = 1; i < n; i++) {
+            if(max > array[i]){
                 max = array[i];
             }
         }
