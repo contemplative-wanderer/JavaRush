df14b8334927ffd0dd79797fa623e4d13a93537c
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-10-26 19:18:41 +0300

    6.3 Massiv ot 10 do 1

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/oct26/L05.java
@@ -0,0 +1,18 @@
+package com.danilenko.helloworld.JavaRush.Lev3.oct26;
+
+/*Заполнение массива из 10 чисел числами от 10 до 1*/
+
+import java.util.Arrays;
+
+public class L05 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            array [i] = 10 - i;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
