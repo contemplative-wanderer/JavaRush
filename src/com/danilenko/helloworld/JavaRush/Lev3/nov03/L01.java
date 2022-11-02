bae1f4b96060c243bf281856eeb6fd2d2cfb92b1
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-11-03 00:28:20 +0200

    Methods. Universal Soldier

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/nov03/L01.java
@@ -0,0 +1,44 @@
+package com.danilenko.helloworld.JavaRush.Lev3.nov03;
+
+/*Универсальный солдат
+Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+Параметры, которые они должны принимать, придумай сам.
+Требования:
+Программа должна содержать 10 методов с именем universalMethod.*/
+
+public class L01 {
+
+    public static void main(String[] args) {
+    }
+
+    public static void universalMethod() {
+    }
+
+    public static void universalMethod(String b) {
+    }
+
+    public static void universalMethod(boolean c) {
+    }
+
+    public static void universalMethod(char d) {
+    }
+
+    public static void universalMethod(double f) {
+    }
+
+    public static void universalMethod(long g) {
+    }
+
+    public static void universalMethod(boolean h, boolean v) {
+    }
+
+    public static void universalMethod(String k, String y) {
+    }
+
+    public static void universalMethod(char q, char w) {
+    }
+
+    public static void universalMethod(int q, String w) {
+    }
+}
