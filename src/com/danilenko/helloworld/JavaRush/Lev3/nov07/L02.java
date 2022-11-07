0484612a6b9827201d851ca834cdb0f97b38e473
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-11-08 00:22:02 +0200

    Minimum integer

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/nov07/L02.java
@@ -0,0 +1,9 @@
+package com.danilenko.helloworld.JavaRush.Lev3.nov07;
+
+public class L02 {
+
+    byte a = Byte.MIN_VALUE;
+    short b = Short.MIN_VALUE;
+    int c = Integer.MIN_VALUE;
+    long d = Long.MIN_VALUE;
+}
