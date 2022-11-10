ca44f09b4b24c6e98adf9a129339c994cd8e8250
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-11-10 20:51:24 +0200

    First objects

--- src/com/danilenko/helloworld/JavaRush/Lev3/nov10/L01.java
+++ src/com/danilenko/helloworld/JavaRush/Lev3/nov10/L01.java
@@ -1,4 +1,36 @@
 package com.danilenko.helloworld.JavaRush.Lev3.nov10;
 
+/*Первые объекты
+Используя оператор new в методе main(), создай:
+10 объектов типа String;
+5 объектов типа int[] (массивов значений int);
+2 объекта типа Scanner.
+Требования:
+В методе main() должно создаваться 10 объектов типа String.
+В методе main() должно создаваться 5 объектов типа int[].
+В методе main() должно создаваться 2 объекта типа Scanner.*/
+
+import java.util.Scanner;
+
 public class L01 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String a0 = new String();
+        String a1 = new String();
+        String a2 = new String();
+        String a3 = new String();
+        String a4 = new String();
+        String a5 = new String();
+        String a6 = new String();
+        String a7 = new String();
+        String a8 = new String();
+        String a9 = new String();
+        int[] b0 = new int[10];
+        int[] b1 = new int[10];
+        int[] b2 = new int[10];
+        int[] b3 = new int[10];
+        int[] b4 = new int[10];
+        Scanner c0 = new Scanner(System.in);
+        Scanner c1 = new Scanner(System.in);
+    }
 }
