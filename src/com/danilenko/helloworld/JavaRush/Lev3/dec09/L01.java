eae6cfb3e1fe84f576304c1bcfc9d258c92f128b
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2022-12-09 21:14:44 +0200

    Class Names - Abbreviation

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/dec09/L01.java
@@ -0,0 +1,15 @@
+package com.danilenko.helloworld.JavaRush.Lev3.dec09;
+
+/*В классе Solution замени все короткие имена классов в коде на полные.
+Требования:
+В методе main должны быть только полные имена классов: остальное менять нельзя.
+В классе Solution не должно быть импортов.*/
+
+public class L01 {
+
+    public static void main(String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
