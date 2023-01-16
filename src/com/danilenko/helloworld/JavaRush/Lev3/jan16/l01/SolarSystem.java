8249aaf02b20f467538f332285e1d7bd65976c7e
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2023-01-16 16:31:40 +0200

    The solar system is our home

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/jan16/l01/SolarSystem.java
@@ -0,0 +1,13 @@
+package com.danilenko.helloworld.JavaRush.Lev3.jan16.l01;
+
+public class SolarSystem {
+    public static long age = 4_568_200_000L;
+
+    public static int planetsCount = 8;
+
+    public static int starsCount = 1;
+
+    public static String starName = "Солнце";
+
+    public static int galacticCenterDistance = 27170;
+}
