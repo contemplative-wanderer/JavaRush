3c1ca9355d3f6b827a5463c10342645af42c5e02
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2023-01-17 17:50:30 +0200

    Presentation of robots

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/dec23/l03/Robot.java
@@ -0,0 +1,25 @@
+package com.danilenko.helloworld.JavaRush.Lev3.dec23.l03;
+
+public class Robot {
+
+    public static int robotCounter;
+
+    public int id;
+
+    public String model;
+
+    public  int productionYear;
+
+    public static String vendor = "Javarush Co.";
+
+    public Robot(String model, int productionYear) {
+        this.model = model;
+        this.productionYear = productionYear;
+        this.id = ++robotCounter;
+    }
+
+    public void displayInformation() {
+        System.out.printf("Я %d-й из %d роботов. Модель: %s. Произведен компанией %s в %d году.\n",
+                id, robotCounter, model, vendor, productionYear);
+    }
+}
