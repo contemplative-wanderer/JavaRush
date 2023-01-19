616a6e527dff818b7e6a65797631db0782fb9edb
contemplative-wanderer <112903223+contemplative-wanderer@users.noreply.github.com>
2023-01-20 01:44:34 +0200

    MacCoffee 3 в 1

---
+++ src/com/danilenko/helloworld/JavaRush/Lev3/dec28/L04Coffee.java
@@ -0,0 +1,16 @@
+package com.danilenko.helloworld.JavaRush.Lev3.dec28;
+
+/*MacCoffee 3 в 1
+У нас есть набор классов, чтобы сделать кофе три в одном, но почему-то кофе не заваривается — задача не компилируется. Найди и исправь одну ошибку в коде.
+Требования:
+В файле Coffee.java должен быть класс Coffee.
+В файле Coffee.java должен быть класс Sugar.
+В классе Coffee должен быть public static класс Milk.
+Программа должна компилировать*/
+
+public class L04Coffee {
+    public static class Milk {
+    }
+}
+class Sugar {
+}
